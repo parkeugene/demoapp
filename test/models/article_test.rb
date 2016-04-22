2be2e1c95712859517b6111b682bb17455b096fd
Eugene Park <parkeugene7676@gmail.com>
2016-04-22 17:23:29 +0900

    Add message parameter

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -7,6 +7,6 @@ class ArticleTest < ActiveSupport::TestCase
 
   test "should not save article without title" do
     article = Article.new
-    assert_not article.save
+    assert_not article.save, "Saved the article without a title"
   end
 end
